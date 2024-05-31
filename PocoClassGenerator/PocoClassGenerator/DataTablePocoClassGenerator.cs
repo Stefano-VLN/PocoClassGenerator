5772fca2f46234fce3f09e1bea81e5c7d7b331f2
Stefano-VLN <52669275+Stefano-VLN@users.noreply.github.com>
2024-05-31 10:44:58 +0200

    Apply VS2022 messages suggestions

--- PocoClassGenerator/PocoClassGenerator/DataTablePocoClassGenerator.cs
+++ PocoClassGenerator/PocoClassGenerator/DataTablePocoClassGenerator.cs
@@ -3,38 +3,42 @@ using System.Collections.Generic;
 using System.Data;
 using System.Text;
 
-public static partial class DataTablePocoClassGenerator
+namespace PocoClassGenerator
 {
-    private static readonly Dictionary<Type, string> TypeAliases = new Dictionary<Type, string> {
-       { typeof(int), "int" },
-       { typeof(short), "short" },
-       { typeof(byte), "byte" },
-       { typeof(byte[]), "byte[]" },
-       { typeof(long), "long" },
-       { typeof(double), "double" },
-       { typeof(decimal), "decimal" },
-       { typeof(float), "float" },
-       { typeof(bool), "bool" },
-       { typeof(string), "string" }
-    };
-
-    public static string GetTypeAliase(System.Type type)
+    public static partial class DataTablePocoClassGenerator
     {
-        if (TypeAliases.TryGetValue(type, out string typename))
-            return typename;
-        else
-            return type.FullName;
-    }
+        private static readonly Dictionary<Type, string> TypeAliases = new()
+        {
+            { typeof(int), "int" },
+            { typeof(short), "short" },
+            { typeof(byte), "byte" },
+            { typeof(byte[]), "byte[]" },
+            { typeof(long), "long" },
+            { typeof(double), "double" },
+            { typeof(decimal), "decimal" },
+            { typeof(float), "float" },
+            { typeof(bool), "bool" },
+            { typeof(string), "string" }
+        };
 
-    public static string GenerateClass(this DataTable dt)
-    {
-        var datas = new List<Dictionary<string, System.Type>>();
-        var sb = new StringBuilder();
-        sb.AppendLine($"public class {dt.TableName}");
-        sb.AppendLine("{");
-        foreach (DataColumn dc in dt.Columns)
-            sb.AppendLine($"	public {GetTypeAliase(dc.DataType)} {dc.ColumnName} {{ get; set; }}");
-        sb.Append("}");
-        return sb.ToString();
+        public static string GetTypeAliase(Type type)
+        {
+            if (TypeAliases.TryGetValue(type, out string typename))
+                return typename;
+            else
+                return type.FullName;
+        }
+
+        public static string GenerateClass(this DataTable dt)
+        {
+            //var datas = new List<Dictionary<string, Type>>();
+            var sb = new StringBuilder();
+            sb.AppendLine($"public class {dt.TableName}");
+            sb.AppendLine("{");
+            foreach (DataColumn dc in dt.Columns)
+                sb.AppendLine($" public {GetTypeAliase(dc.DataType)} {dc.ColumnName} {{ get; set; }}");
+            sb.Append('}');
+            return sb.ToString();
+        }
     }
 }
