dd77131bae022d417a5be706a976d688399b1c7a
Stefano Deluca <stefano.deluca@velan.com>
2025-09-05 09:10:46 +0200

    Update Program.cs

--- PocoClassGenerator/PocoClassGeneratorRunner/Program.cs
+++ PocoClassGenerator/PocoClassGeneratorRunner/Program.cs
@@ -7,7 +7,7 @@ namespace PocoClassGeneratorRunner
     {
         static void Main()
         {
-            using var conn = new SqlConnection(@"Server = ABV-DB-03;Database = DATAOPS;Trusted_Connection=True;");
+            using var conn = new SqlConnection(@"Server = ABV-DB-03;Database = DATAOPS;Trusted_Connection=True;Encrypt=False");
             var Pocos = conn.GenerateAllTables(GeneratorBehavior.View);
             File.WriteAllText("Pocos.cs", Pocos);
         }
